fb6537baf6d52ff6ad379b028992d1ca3ae3bd50
veerendra.toleti <veerendra.toleti@sonata-software.com>
2019-12-29 13:41:15 +0530

    asgfasf

--- iam/get_all_iam_users.py
+++ iam/get_all_iam_users.py
@@ -1,5 +1,7 @@
 import boto3
 iam = boto3.client('iam')
+count = 0
 for user in iam.list_users()['Users']:
-    print("Users: {0}\nUserID: {1}\n ARN: {2}".format(user['UserName'],user['UserId'],user['Arn']))
+    print("Users:= {0}\nUserID: {1}\n ARN: {2}".format(user['UserName'],user['UserId'],user['Arn']))
+    count = count+1
     
