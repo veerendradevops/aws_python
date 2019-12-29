4a7d78161b489b92b5854eefa5f69eff571bdabf
veerendra.toleti <veerendra.toleti@sonata-software.com>
2019-12-29 12:59:41 +0530

    added

---
+++ iam/get_iam_user_details.py
@@ -0,0 +1,11 @@
+import boto3
+import pprint
+#session=boto3.session.Session(profile_name="default")
+#re_iam=session.resource(service_name="iam",region_name="us-east-1")
+re_iam=boto3.resource(service_name="iam")
+iam_user_ob=re_iam.User("EC2_S3_IAM")
+#print(dir(iam_user_ob))
+pprint.pprint(dir(iam_user_ob))
+print "IAM Details"
+print "==========="
+print "UserName:{}"
