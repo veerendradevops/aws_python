013eea0f30a38c64a5ad9897f2ab1eab693ac333
veerendra.toleti <veerendra.toleti@sonata-software.com>
2019-12-29 16:45:46 +0530

    sdfsad

--- iam/filter_users_based_on_tags.py
+++ iam/filter_users_based_on_tags.py
@@ -7,4 +7,10 @@ policyarn = []
 
 #find each policy attached to the user
 
-print("***********")
+for policy in user_detail['AttachedManagedPolicies']:
+ policyname.append(policy['PolicyName'])
+ policyarn.append(policy['PolicyArn'])
+
+ #print user details
+
+ print("User: {0}\nUserID: {1}\nPolicyName: {2}\nPolicyARN: {3}\n".format(user_detail['UserName'],user_detail['UserId'],policyname,policyarn))
