a66f5e11c9ff9354d7e5350e955c22c190a5c84c
veerendra.toleti <veerendra.toleti@sonata-software.com>
2019-12-29 16:21:41 +0530

    dfsg

--- iam/filter_users_based_on_tags.py
+++ iam/filter_users_based_on_tags.py
@@ -1,3 +1,8 @@
 import boto3
 iam = boto3.client('iam')
-print(dir(iam))
+#print(dir(iam))-
+#for user_detail in iam.get_account_authorization_details(Filter=['User'])['UserDetails']:
+
+
+for user_detail in iam.get_account_authorization_details():
+    print(dir(user_detail))
