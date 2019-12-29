eae11ecc89549bed22a4074e5791cffc511c623f
veerendra.toleti <veerendra.toleti@sonata-software.com>
2019-12-29 16:49:57 +0530

    dfsg

--- iam/filter_users_based_on_tags.py
+++ iam/filter_users_based_on_tags.py
@@ -1,16 +1,18 @@
 import boto3
 iam = boto3.client('iam')
-#print(dir(iam))-
-for user_detail in iam.get_account_authorization_details(Filter=['User'])['UserDetails']:
-policyname = []
-policyarn = []
 
-#find each policy attached to the user
-
-for policy in user_detail['AttachedManagedPolicies']:
+for user_detail in iam.get_account_authorization_details(Filter=['User'])['UserDetailList']:
+ policyname = []
+ policyarn = []
+ # find each policy attached to the user
+ for policy in user_detail['AttachedManagedPolicies']:
  policyname.append(policy['PolicyName'])
  policyarn.append(policy['PolicyArn'])
-
- #print user details
-
- print("User: {0}\nUserID: {1}\nPolicyName: {2}\nPolicyARN: {3}\n".format(user_detail['UserName'],user_detail['UserId'],policyname,policyarn))
+ # print user details 
+ print("User: {0}\nUserID: {1}\nPolicyName: {2}\nPolicyARN: {3}\n".format(
+ user_detail['UserName'],
+ user_detail['UserId'],
+ policyname,
+ policyarn
+ )
+ )
