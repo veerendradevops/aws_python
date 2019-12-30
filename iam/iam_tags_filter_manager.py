2a67fc41d7698a855504e23f74cf0c129c05f06d
veerendradevops <veerendratholeti@gmail.com>
2019-12-30 16:19:31 +0530

    sadfsad

---
+++ iam/iam_tags_filter_manager.py
@@ -0,0 +1,47 @@
+import json
+import logging
+import boto3
+
+#profile = 'mycloud'
+region = 'us-east-1'
+#boto3.setup_default_session(profile_name=profile, region_name=region)
+file_1 = 'C:\\Users\\veerendra.toleti\\Documents\\test.json'
+tag_user = []
+non_tag_user = []
+userlist = []
+
+#Define Boto3 Clients
+
+iam = boto3.client('iam', region_name=region)
+
+#List Users
+
+paginator = iam.get_paginator('list_users')
+
+user_list = paginator.paginate()
+
+for ulist in user_list:
+
+    for user in ulist['Users']:
+
+        x = user['UserName']
+
+        userlist.append(x)
+
+print(userlist)        
+
+for uname in userlist:
+
+    user_tags = iam.list_user_tags(UserName=uname)['Tags']
+
+    for i in user_tags:
+
+            if 'Manager' in i['Key']:
+
+                tag_user.append(uname)
+
+print(tag_user)
+
+non_tag_user = [item for item in userlist if item not in tag_user]
+
+print(non_tag_user)
