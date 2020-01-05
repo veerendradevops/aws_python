c7c2767d806396494d412453f8d07383f1d09ee0
veerendra.toleti <veerendra.toleti@sonata-software.com>
2020-01-05 13:16:12 +0530

    ggfdsfg

--- iam_role/create_role.py
+++ iam_role/create_role.py
@@ -1,7 +1,7 @@
 import boto3
 client = boto3.client('iam')
 responcse = client.update_role(
-   RoleName='vk_aws_cloudformation_role'
-   Description='Allow Vk'
+   RoleName='vk_aws_cloudformation_role',
+   Description='Allow Vk',
    MaxSessionDuration=7200
 )
