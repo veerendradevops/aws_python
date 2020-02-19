149ebe6fc0a381ed56cdd96770bc5e593c216bac
veerendra.toleti <veerendra.toleti@sonata-software.com>
2020-02-19 23:38:56 +0530

    Creating_ES_serviceRole

---
+++ ec2/start_instances_scheduled.py
@@ -0,0 +1,24 @@
+import boto3
+
+session=boto3.session.Session(profile_name="default")
+ec2=boto3.client("ec2")
+
+response2 = ec2.describe_tags(
+    Filters=[
+        {
+            'Name': 'tag:Name',
+            'Values': [
+                'Test',
+            ]
+        }
+    ]
+    )
+#print(response2)
+
+for i in response2['Tags']:
+    in_id = (i['ResourceId'])
+    print(in_id)
+    ec2.start_instances(InstanceIds=[in_id])
+    
+
+
