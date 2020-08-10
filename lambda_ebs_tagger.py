afa5b562c4c3dc511842e2eca2c52fa21562aa3f
veerendradevops <37468369+veerendradevops@users.noreply.github.com>
2020-08-10 16:48:44 +0530

    Update lambda_ebs_tagger.py

--- lambda_ebs_tagger.py
+++ lambda_ebs_tagger.py
@@ -1,3 +1,5 @@
+
+##################Lambda Function ############################
 import json
 import boto3
 
@@ -33,3 +35,34 @@ def lambda_handler(event, context):
                                 vol.create_tags(Tags=tags_to_copy)
                     else:
                         print("No tags available to copy")
+                                      
+     ##################Python script without lambda ##########################3
+import json
+import boto3
+
+application = "test"
+#resource=boto3.session.Session(profile_name="veeren",region_name="us-east-1")
+#ec2_resou=session.resource(service_name="ec2",region_name="us-east-1")
+boto3.setup_default_session(profile_name="veeren", region_name="us-east-1")
+instances = boto3.resource('ec2').instances.all()
+tags_to_be_copied = []
+
+for ec2 in instances:
+    temp = ec2.tags
+    for tag in temp:
+        if tag["Value"] == application:
+            tag_value = tag["Value"]
+            print(tag_value)
+            tags_to_be_copied = ["GSID"]
+            if len(tags_to_be_copied) != 0:
+                for instance in instances:
+                    tags_to_copy = [tag for tag in instance.tags
+                                        if tag["Key"] in tags_to_be_copied] if instance.tags else[]
+                    if not tags_to_copy:
+                        continue
+                    #print(f"{instance.instance_id}:{instance.tags}")
+                    for vol in instance.volumes.all():
+                        #print(f"{vol.attachments[0]['Device']}: {tags_to_copy}")
+                        vol.create_tags(Tags=tags_to_copy)
+            else:
+                print("No tags available to copy")
